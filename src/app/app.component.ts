7769fc2aacdfc5eb9b87a8b46694cd73655f954b
Abzed <ibraabzed@gmail.com>
2020-08-22 13:41:06 +0300

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
-import $ from "jquery";
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'quotes';
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    $(document).ready(function(){
-      $("h2").click(function(){
-        alert("success")
-      });
-    });
-  }
+  ngOnInit(): void {}
 }
