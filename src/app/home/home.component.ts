8b206dfec4bf28394a63ac5db6da23428a5c0503
Abzed <ibraabzed@gmail.com>
2020-08-22 17:09:06 +0300

    add typescript function for famous quotes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,19 +7,24 @@ import $ from 'jquery';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  quoteOne = "The way to get started is to quit talking and begin doing";
+  quoteTwo = "Tell me and I forget. Teach me and I remember. Involve me and I learn";
+  quoteThree = "Whoever is happy will make others happy too";
   
   constructor() { }
 
   ngOnInit(): void {
     $(document).ready(function(){
+      
       $("#addtoquotes").submit(function(){
         let inputQuote = $("#quoteinput").val();
-        let key = 'Item 1';
-         localStorage.setItem(key, 'Value');        
+         localStorage.setItem(inputQuote, `${inputQuote}`);        
         console.log(inputQuote);
-        
-        
+        let display = $("#quotes");
+        display.append('<p>' + '<i class="fa fa-quote-left"></i>' + " " + localStorage.getItem(inputQuote) + " " + '<i class="fa fa-quote-right"></i>' + '</p>');
 
+        event.preventDefault();
+               
       })
      
     })
