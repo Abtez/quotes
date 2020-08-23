cf950d20c4b5d16d25a629f2b0e891df71a89514
Abzed <ibraabzed@gmail.com>
2020-08-23 07:34:55 +0300

    add upvote and downvote functon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from 'jquery';
 
 
 @Component({
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quotes';
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    $(document).ready(function(){
+      
+      $("#addtoquotes").submit(function(){
+        let inputQuote = $("#quoteinput").val();
+         localStorage.setItem(inputQuote, `${inputQuote}`);        
+        console.log(inputQuote);
+        let newQuote = localStorage.getItem(inputQuote);
+
+        $(".my-quotes").html('<p><i class="fa fa-quote-left"></i>' + " " + newQuote + " " +
+         '<i class="fa fa-quote-right"></i>' + "<span class='icons' style='display: flex;justify-content: space-evenly;padding: 7px;cursor: pointer;font-size: 14px;'><i class='far fa-thumbs-up fa-2x' style='color: green;'><span class='counter'>22</span></i><i class='far fa-thumbs-down fa-2x' style='color: rgba(255, 102, 0, 0.637);'><span class='counter'>22</span></i><i class='far fa-trash-alt fa-2x' style='color: red;'></i></span>" +
+          " " + '</p>').addClass("myquotes").append("<hr>");
+
+        event.preventDefault();
+               
+      });
+     
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import $ from 'jquery';
 
@@ -10,25 +11,24 @@ export class HomeComponent implements OnInit {
   quoteOne = "The way to get started is to quit talking and begin doing";
   quoteTwo = "Tell me and I forget. Teach me and I remember. Involve me and I learn";
   quoteThree = "Whoever is happy will make others happy too";
-  
-  constructor() { }
 
-  ngOnInit(): void {
-    $(document).ready(function(){
-      
-      $("#addtoquotes").submit(function(){
-        let inputQuote = $("#quoteinput").val();
-         localStorage.setItem(inputQuote, `${inputQuote}`);        
-        console.log(inputQuote);
-        let newQuote = localStorage.getItem(inputQuote);
+  textsize = '25px'
 
-        $(".my-quotes").addClass("myquotes").append('<p><i class="fa fa-quote-left"></i>' + " " + newQuote + " " + '<i class="fa fa-quote-right"></i>' + "<span class='icons' style='display: flex;justify-content: space-evenly;padding: 7px;cursor: pointer;font-size: 14px;'><i class='far fa-thumbs-up fa-2x' style='color: green;'><span class='counter'>22</span></i><i class='far fa-thumbs-down fa-2x' style='color: rgba(255, 102, 0, 0.637);'><span class='counter'>22</span></i><i class='far fa-trash-alt fa-2x' style='color: red;'></i></span>" + " " + '</p>').append("<hr>");
+  thumbsup = 0;
+  thumbsdown = 0;
 
-        event.preventDefault();
-               
-      })
-     
-    })
+  dislike(){
+    this.thumbsdown = this.thumbsdown+1;
+  }
+
+  like(){
+    this.thumbsup = this.thumbsup+1;
+  }
+
+  constructor() {
+   }
+
+  ngOnInit(): void { 
   }
 
 }
