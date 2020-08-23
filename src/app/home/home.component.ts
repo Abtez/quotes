251ec144e8aa1c25342250f1a96d70333cb062a5
Abzed <ibraabzed@gmail.com>
2020-08-23 09:29:21 +0300

    add delete function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,18 @@
 import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import $ from 'jquery';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  quoteOne = "The way to get started is to quit talking and begin doing";
-  quoteTwo = "Tell me and I forget. Teach me and I remember. Involve me and I learn";
-  quoteThree = "Whoever is happy will make others happy too";
-
-  textsize = '25px'
+  quotes = [ "The way to get started is to quit talking and begin doing",
+  "Tell me and I forget. Teach me and I remember. Involve me and I learn",
+  "Whoever is happy will make others happy too"
+]
 
   thumbsup = 0;
   thumbsdown = 0;
@@ -29,6 +29,11 @@ export class HomeComponent implements OnInit {
    }
 
   ngOnInit(): void { 
+    $(document).ready(function(){
+      $(".delete").click(function(){
+        $(".items").remove();
+      })
+    })
   }
 
 }
